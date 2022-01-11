e92375cdf11b8d7df19390149dd33db3aceca176
Caleb Goh <47849515+calebanana@users.noreply.github.com>
2022-01-12 00:05:17 +0800

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,6 +5,13 @@ def test_Addition():
 
 def test_Pass():
     assert add1(1) == 2
+    
+def test_Pass2():
+    assert add1(5) == 6
 
 def test_Failing():
     assert add1(2) == 4
+
+@pytest.mark.skip()
+def test_Skip():
+    assert True
